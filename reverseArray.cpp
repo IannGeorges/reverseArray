58b41aabaa86dbc67b8a2204b3f83ce2169173e4
Shivam Patidar <patidarshivam212@gmail.com>
2021-05-20 01:26:08 +0530

    Simple way to reverse an array

--- reverseArray.cpp
+++ reverseArray.cpp
@@ -6,31 +6,36 @@
 using namespace std;
 
 
-int main() {
-    // initialize variables
-    int a, b, c, size, last;
-    // initialize array size with the user's input
-    int array[size];
-    // makes the rand() function change integers everytime the program is ran 
-    srand(time(NULL));
-    // prompt the user for the array size
-    cout << "Enter size of array \n";
-    // retrieve user input
-    cin >> size;
-    // for loop to populate the array with random numbers
-    for (a = 0;a<size;a++)
+void reverse (int array[],int size)
+{
+    int start = 0;  //starting position that is 0
+    int end = size-1;   // initialize the variable at the last address of the array
+    
+    while(start<end)
     {
-        b = rand() % size+1;
-        array[a]=b;
-        cout << array[a];
+        int temp = array[start];  // entire code is to swap first and last value and increase value of start and decrease value of end and again do the same.
+        array[start] = array[end]; 
+        array[end] = temp;  
+        
+        start++;
+        end--;
     }
-    // initialize the variable at the last address of the array
-    last = size - 1;
-    cout << "\n";
-    // for loop to print the reverse of the array
-    for (c = last; c >= 0; c--)
-    {
-        cout << array[c];
-    }  
-    return 0;
+    
+    cout<<"After reversing the array elements :\n";
+    for(int i=0;i<size;i++)
+        cout<<array[i]<<" ";   //printing array elements in reverse order
+}
+
+int main() {
+   int size; // size of an array
+   cout << "Enter size of array \n";
+   cin>>size; // taking input from user
+    
+   int array[size]; //defining array of user's size
+   
+   cout<<"Enter elements of array \n";
+   for(int i=0;i<size;i++) // taking array input from user
+      cin>>array[i];
+    
+   reverse (array,size);  //calling reverse function to reverse array elements 
 }
